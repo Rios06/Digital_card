915e0e445143f7d2ad997771eef15482fed6d911
Rios06 <jenniriioss4@gmail.com>
2024-01-18 10:18:49 -0500

    feat(about): added personal information about myself to the about file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Info from './components/info';
+import About from './components/about';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Info />
+      <About/>
     </div>
   );
 }
---
+++ src/components/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <p>Hola, soy Rios, una persona proactiva que disfruta aprender nuevas cosas con una pasion en
+         el trabajo en equipo y la busqueda de soluciones innovadoras para optimizar proyectos</p>
+    </div>
+  );
+};
+
+export default About;
