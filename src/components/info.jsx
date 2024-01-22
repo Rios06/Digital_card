6bce36f616f1898f1a1c4243aef5f318a1438cb3
Rios06 <jenniriioss4@gmail.com>
2024-01-21 21:44:21 -0500

    chore(info): add comments to display profile information and social media links

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,3 +1,4 @@
+// Info component for displaying profile information and social media links
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/fontawesome-free-brands';
@@ -6,6 +7,7 @@ import fotoP from "../assets/profile-photo.png";
 const Info = () => {
   return (
     <div className="info">
+        {/* Displaying profile information and social media links */}
       <img src={fotoP} alt="Profilephoto" className="profilephoto" />
       <h1>Jennifer Rios</h1>
       <div className="buttons">
